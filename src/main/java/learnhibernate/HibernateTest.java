11f2ba1b1b991bb357e7b00de92c431246e7c9ed
zzanthia <zanthia@tut.by>
2018-08-14 00:37:39 +0300

    Lessons 1-5

---
+++ src/main/java/learnhibernate/HibernateTest.java
@@ -0,0 +1,22 @@
+package learnhibernate;
+
+import learnhibernate.dto.UserDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateTest {
+
+    public static void main(String[] args) {
+        UserDetails user = new UserDetails();
+        user.setUserId(1);
+        user.setUserName("First User");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+    }
+
+}
