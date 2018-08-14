dd7366e0545d7d9fd53645a151e97cafb04fa158
lena <zanthia@tut.by>
2018-08-14 16:10:31 +0300

    Videos 6-8. Annotations: Table, Column, Transient, Temporal, Lob Session.get() hibernate.hbm2ddl.auto = create/update

--- src/main/java/learnhibernate/HibernateTest.java
+++ src/main/java/learnhibernate/HibernateTest.java
@@ -5,18 +5,31 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 public class HibernateTest {
 
     public static void main(String[] args) {
         UserDetails user = new UserDetails();
         user.setUserId(1);
         user.setUserName("First User");
+        user.setAddress("First User's address");
+        user.setJoinedDate(new Date());
+        user.setDesciption("Description of the user goes here");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(user);
         session.getTransaction().commit();
+        session.close();
+
+        user = null;
+        session = sessionFactory.openSession();
+        session.beginTransaction();
+        user = session.get(UserDetails.class, 1);
+        System.out.println("User: "+user);
+
     }
 
 }
--- src/main/java/learnhibernate/dto/UserDetails.java
+++ src/main/java/learnhibernate/dto/UserDetails.java
@@ -1,14 +1,27 @@
 package learnhibernate.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
+//@Entity(name = "USER_DETAILS")
 @Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
 
     @Id
+    //@Column(name = "USER_ID")
     private int userId;
+
+    //@Column(name = "USER_NAME")
+    //@Transient
     private String userName;
+
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    private String address;
+
+    @Lob
+    private String desciption;
 
     public int getUserId() {
         return userId;
@@ -26,4 +39,38 @@ public class UserDetails {
         this.userName = userName;
     }
 
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDesciption() {
+        return desciption;
+    }
+
+    public void setDesciption(String desciption) {
+        this.desciption = desciption;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetails{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", joinedDate=" + joinedDate +
+                ", address='" + address + '\'' +
+                ", desciption='" + desciption + '\'' +
+                '}';
+    }
 }
